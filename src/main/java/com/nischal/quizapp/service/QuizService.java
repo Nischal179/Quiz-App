27c974997e577f5d19838c1823a5a614ac235d87
Nischal876 <nischalaryal11@gmail.com>
2024-09-17 15:50:27 +0545

    feat: add QuestionWrapper and implement quiz question retrieval
    - Added QuestionWrapper class to encapsulate quiz question details with multiple options.
    - Implemented getQuizQuestions method in QuizService to retrieve and wrap questions for user display.
    - Added new GET endpoint in QuizController to fetch quiz questions by quiz ID.

--- src/main/java/com/nischal/quizapp/service/QuizService.java
+++ src/main/java/com/nischal/quizapp/service/QuizService.java
@@ -3,13 +3,16 @@ package com.nischal.quizapp.service;
 import com.nischal.quizapp.dao.QuestionDao;
 import com.nischal.quizapp.dao.QuizDao;
 import com.nischal.quizapp.model.Question;
+import com.nischal.quizapp.model.QuestionWrapper;
 import com.nischal.quizapp.model.Quiz;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class QuizService {
@@ -30,5 +33,25 @@ public class QuizService {
         quizDao.save(quiz);
 
         return new ResponseEntity<>("Success", HttpStatus.CREATED);
+    }
+
+    public ResponseEntity<List<QuestionWrapper>> getQuizQuestions(Integer id) {
+        Optional<Quiz> quiz = quizDao.findById(id);
+        List<Question> questionsFromDB = quiz.get().getQuestions();
+        List<QuestionWrapper> questionsForUser = new ArrayList<>();
+        for (Question question : questionsFromDB) {
+            QuestionWrapper questionWrapper = new QuestionWrapper(
+                    question.getId(),
+                    question.getQuestionTitle(),
+                    question.getOption1(),
+                    question.getOption2(),
+                    question.getOption3(),
+                    question.getOption4()
+            );
+
+            questionsForUser.add(questionWrapper);
+        }
+
+        return new ResponseEntity<>(questionsForUser, HttpStatus.OK);
     }
 }
